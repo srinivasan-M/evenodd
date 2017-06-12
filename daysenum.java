7d7bbfb9deecf3e03e209102ccf83336a5b059d9
srinivasan-M <srinivasanksr15@gmail.com>
2017-06-13 00:06:19 +0530

    Create daysenum.java

---
+++ daysenum.java
@@ -0,0 +1,42 @@
+import java.util.*;
+class daysenum
+{
+public static void main(String args[])
+{
+int day=7;
+String[] days={"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+switch(day)
+{
+case 1:
+         System.out.println("True");
+         break;
+
+case 2:
+         System.out.println("True");
+         break;
+
+case 3:
+         System.out.println("True");
+         break;
+
+case 4:
+         System.out.println("True");
+         break;
+
+case 5:
+         System.out.println("True");
+         break;
+
+case 6:
+         System.out.println("False");
+         break;
+
+case 7:
+         System.out.println("False");
+         break;
+default:
+         System.out.println("Invalid day");
+         break;
+}
+}
+}
