cd4febd128f39dca0f12ba1d7112020f9729c0c2
srinivasan-M <srinivasanksr15@gmail.com>
2017-06-12 23:21:00 +0530

    Create StoI.java

---
+++ StoI.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class StoI
+{
+public static void main(String args[])
+{
+Scanner ss=new Scanner(System.in);
+String s=ss.nextLine();
+int a=Integer.parseInt(s);
+System.out.print(a);
+}
+}
