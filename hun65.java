679daf88730a0d7f2ae903b82fa30b63ae11d033
srinivasan-M <srinivasanksr15@gmail.com>
2017-06-15 10:26:07 +0530

    Create hun65.java

---
+++ hun65.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class hun65
+{
+public static void main(String args[])
+{
+Scanner ss=new Scanner(System.in);
+String s=ss.nextLine();
+String rpl=s.replaceAll("10","");
+System.out.print(rpl);
+}
+}
