00bb90532c215d9e905d59f15c691430c99e9646
srinivasan-M <srinivasanksr15@gmail.com>
2017-06-23 03:16:47 -0700

    Create hun34.java

---
+++ hun34.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class hun34
+{
+public static void main(String args[])
+{
+Scanner ss=new Scanner(System.in);
+int n=ss.nextInt();
+int l=s.length();
+int l1=s1.length();
+String s=String.valeOf(n);
+n++;
+String s1=String.valueOf(n);
+if(l1>l)
+System.out.print("N0T P0SSIBLE");
+else
+System.out.print(n);
+}
+}
